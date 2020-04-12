5cfa29840b7b9f3bc84b960da42538e08657bae7
Swiggies <38583668+Swiggies@users.noreply.github.com>
2020-04-12 15:47:58 +1000

    feature/US19
    Added player model animations
    Added hit animation

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Blend X", Input.GetAxis("Hor1"));
+        animator.SetFloat("Blend Y", Input.GetAxis("Ver1"));
+        if(Input.GetKeyDown(KeyCode.Space))
+            animator.SetTrigger("Hit");
+    }
+}
