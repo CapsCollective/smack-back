9772a571080591bfbaebfcecf3b270ccb4ea6c4d
Ben Kelly <benkellysoftware@gmail.com>
2020-04-14 17:58:58 +1000

    Fixed paddle colours and tv's to work with the bloom

---
+++ Assets/ScreenToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenToggle : MonoBehaviour
+{
+    public Material mat1, mat2;
+
+    private MeshRenderer _meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        StartCoroutine(Toggle());
+    }
+
+    private IEnumerator Toggle()
+    {
+        while (true)
+        {
+            _meshRenderer.material = mat1;
+            yield return new WaitForSeconds(3);
+            _meshRenderer.material = mat2;
+            yield return new WaitForSeconds(3);
+        }
+    } 
+}
